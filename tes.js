976b3be67d78bb1cbc39e1f77e09cce96475ab0a
trdmrk <mangaonline.xyz@gmail.com>
2017-03-04 22:32:58 +0700

    Delete tes.js

--- tes.js
+++
@@ -1,2 +0,0 @@
-    var posts_no = 6;
-    document.write(&quot;&lt;script src=\&quot;/feeds/posts/default/-/Recommended?orderby=published&amp;alt=json-in-script&amp;callback=showlatestpostswiththumbs\&quot;&gt;&lt;\/script&gt;&quot;);
