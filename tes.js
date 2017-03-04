6ba7b23763842c864a5613ec71c80344b4c98d4e
trdmrk <mangaonline.xyz@gmail.com>
2017-03-04 22:29:39 +0700

    Create tes.js

---
+++ tes.js
@@ -0,0 +1,2 @@
+    var posts_no = 6;
+    document.write(&quot;&lt;script src=\&quot;/feeds/posts/default/-/Recommended?orderby=published&amp;alt=json-in-script&amp;callback=showlatestpostswiththumbs\&quot;&gt;&lt;\/script&gt;&quot;);
